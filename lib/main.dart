70cbb14dbd2d62f30bef50e046ed244c6efe30aa
Swannhs <princeapurbask@gmail.com>
2022-01-11 10:19:46 +0600

    Playing with widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const App());
-}
+void main() => runApp(App());
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text("Hello"),
+    return MaterialApp(
+      title: "Flutter",
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Swann"),
+      ),
+      body: Center(
+        child: Text("This is dummy text"),
+      ),
     );
   }
 }
